cda426d8dfc75e67991d5fca7a1642ab4bfba2b5
naatti16 <148633784+anttitulenheimo@users.noreply.github.com>
2026-06-24 15:12:52 +0300

    Pick-a-pal ready

---
+++ Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Antti Tulenheimo on 24.6.2026.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
